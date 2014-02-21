77e18d0fd2ecee1c2e6bfe530d35cbfc4c765f55
Michael Lai <themichaellai@gmail.com>
2014-02-21 17:07:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drawonshit::Application.config.secret_token = '17fe327849cb6a25cf20f92fb227edf229bf9245c9d298764b94fcea28af1c5a84f156833ca2b042d34e5b98c86d900540a32a88010f9c5e1a197ba4afe9ab41'
