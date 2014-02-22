4c2e0f9d51507ee97fd7caa6cf2ffade2b719864
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-02-21 20:55:29 -0500

    started upvote / downvote

--- app/controllers/doodles_controller.rb
+++ app/controllers/doodles_controller.rb
@@ -19,4 +19,16 @@ class DoodlesController < ApplicationController
   def recent
     @doodles = Doodle.find(order: 'created_at DESC')
   end
+
+  def upvote
+    @doodle = Doodle.find(params[:id])
+    @doodle.liked_by current_user
+    redirect_to :doodles
+  end
+
+  def downvote
+    @doodle = Doodle.find(params[:id])
+    @doodle.downvote_from current_user
+    redirect_to :doodles
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Drawonshit::Application.routes.draw do
   root to: 'doodles#index'
   devise_for :users, :controllers => { omniauth_callbacks:"omniauth_callbacks" }
-  resources :doodles
+  resources :doodles do 
+    member do
+      put "upvote", to: "doodles#upvote" 
+      put "downvote", to: "doodles#downvote" 
+    end
+  end
 end
